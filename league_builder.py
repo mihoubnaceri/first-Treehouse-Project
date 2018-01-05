e7c8ce273f54cbfab2e1fe63405c3bd723024fb7
Mihoub Naceri <lavesolsok@gmail.com>
2018-01-06 00:35:49 +0100

    updated some codes

--- league_builder.py
+++ league_builder.py
@@ -3,10 +3,13 @@ import csv
 
 def main():
     experienced = []
+
     inexperienced = []
     sharks =[] # each team will get 6 players with three experienced players
     dragons = []# each team will get 6 players with three experienced players
     raptors = []# each team will get 6 players with three experienced players
+    teams = [sharks,dragons,raptors]
+    total_players =[]
     with open("soccer_players.csv",newline="") as csvfile:
         rows = csv.DictReader(csvfile)
         for row in rows:
@@ -14,15 +17,25 @@ def main():
                 experienced.append(row) # adding experienced players as objects to the experienced list
             else:
                 inexperienced.append(row) # adding inexperienced players as objects to the inexperienced list
-
     # the exxperienced list has 9 players and also the inesperienced has 9
     # we need to disturbute the 3 from each list to each team
+
     sharks.extend(experienced[:3])
     dragons.extend(experienced[3:6])
     raptors.extend(experienced[6:])
     sharks.extend(inexperienced[:3])
     dragons.extend(inexperienced[3:6])
     raptors.extend(inexperienced[6:])
+    for player in sharks:
+        player.update({"time":"6 am on  7 january 2018" ,"teamname":"Sharks"})
+    for player in dragons:
+        player.update({"time":"2 pm on  9 january 2018" ,"teamname":"Dragons"})
+    for player in raptors:
+        player.update({"time":"9 pm on  4 january 2018" ,"teamname":"Raptors"})
+
+    for team in teams:
+        total_players.extend(team)
+
 
     with open("teams.txt","a") as file:
         file.write("Sharks" + "\n" +"==========" + "\n")
@@ -36,10 +49,15 @@ def main():
         file.write("Raptors" +"\n"+"==========" + "\n")
         for player in raptors:
             file.write("{}, {}, {}" .format(player["Name"],player["Soccer Experience"],player["Guardian Name(s)"]) +"\n")
-    for i in range(18):
+    for player in total_players:
+        name_file = list(player["Name"])
+        index = name_file.index(" ")
+        name_file.insert(index,"_")
+        name_file.remove(" ")
+        with open("{}.txt".format("".join(name_file)),"a" ) as player_file:
+            player_file.write("Dear {}, \n".format(player["Guardian Name(s)"]))
+            player_file.write("Hello {}, {} Welcome to the {} team big welcome your training starts at {}".format(player["Name"],player["Guardian Name(s)"],player["teamname"],player["time"]))
 
-        with open("{}.txt".format("chafik"),"a") as file_save:
-            file_save.write("kiwee")
 
 if __name__ == "__main__":
     main()
